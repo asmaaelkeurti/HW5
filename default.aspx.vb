2837a3deef1c6567a6b372c4a3372e91266110d7
Weijun Li <weijun-li@uiowa.edu>
2014-10-08 21:13:44 -0500

    payment date added, new balance added, css modified

--- default.aspx.vb
+++ default.aspx.vb
@@ -21,6 +21,7 @@ Partial Class _Default
         Dim table As DataTable = New DataTable("ParentTable")
         Dim loanAmortTbl As DataTable = New DataTable("AmortizationTable")
         Dim tRow As DataRow
+        Dim pDate As Date = Date.Now
 
         'This section adds default values to the variables.  
         interestPaid = 0.0
@@ -48,8 +49,10 @@ Partial Class _Default
 
         'Adds items to list box, formats them for currency and adds pad spacing for each item.
         loanAmortTbl.Columns.Add("Payment Number", System.Type.GetType("System.String"))
+        loanAmortTbl.Columns.Add("Payment Date", System.Type.GetType("System.String"))
         loanAmortTbl.Columns.Add("Principal Paid", System.Type.GetType("System.String"))
         loanAmortTbl.Columns.Add("Interest Paid", System.Type.GetType("System.String"))
+        loanAmortTbl.Columns.Add("New Balance", System.Type.GetType("System.String"))
 
 
         'This section uses the for loop to display the loan balance and interest paid over the term of the loan.
@@ -62,12 +65,15 @@ Partial Class _Default
             principal = monthlyPayment - interestPaid
             nBalance = loanAmount - principal
             loanAmount = nBalance
+            pDate = DateAdd(DateInterval.Month, 1, pDate)
 
             'Writes the data to a new row in the gridview.
             tRow = loanAmortTbl.NewRow()
             tRow("Payment Number") = String.Format(counterStart)
+            tRow("Payment Date") = String.Format("{0:MM/dd/yyyy}", pDate)
             tRow("Principal Paid") = String.Format("{0:C}", principal) ' String.Format("{0:C},principal) formats the variable "prinicpal" as currency (C).
             tRow("Interest Paid") = String.Format("{0:C}", interestPaid)
+            tRow("New Balance") = String.Format("{0:C}", nBalance)
             loanAmortTbl.Rows.Add(tRow)
 
             'Loops to next counterStart (Continues loop until counterStart requirements are met (loanTerm)).
